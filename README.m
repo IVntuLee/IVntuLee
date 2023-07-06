da2bebe9a5d1203fcf53ae443bfe163f789352a8
IVntuLee <138771743+IVntuLee@users.noreply.github.com>
2023-07-06 11:47:29 +0200

    Welcome!

--- README.m
+++ README.m
@@ -3,8 +3,3 @@ Welcome 👋 Hi, I’m @IVntuLee
 - 🌱 I’m currently learning the basics
 - 💞️ I’m looking to collaborate with others going forward 
 - 📫 How to reach me? just over here on that digital mailbox 👈🏾
-
-<!---
-IVntuLee/IVntuLee is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
